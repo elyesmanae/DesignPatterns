0cc411b8819124203dad190694913b5c799bcc20
elyesmanae <elyesmanae@gmail.com>
2021-04-12 17:23:54 +0200

    DP_Builder

--- DesignPatterns/src/Builder/Builder.java
+++ DesignPatterns/src/Builder/Builder.java
@@ -1,5 +1,9 @@
 package Builder;
 
+/**
+ * Builder interface defines all possible ways to configure a product.
+ */
+
 public interface Builder {
 	void setCarType(CarType type);
 	void setSeats(int seats);
